217d6663870a58d6cad591843fa7b390388771f0
SergioETD <sergio.tenorio@polimentes.com>
2018-10-23 13:08:59 -0500

    se borraron prints serializer

--- libreria/libreria/serializers.py
+++ libreria/libreria/serializers.py
@@ -13,21 +13,12 @@ class ValidateSerializer(serializers.Serializer):
     genero = serializers.CharField(max_length=10)
 
     def validate_genero(self, param):
-        print("param de validate_genero: " + param)
         if param == "Terror" or param == "Novela" or param == "Comedia":
-            print("Param ok")
             return param
         else:
-            print("param mal")
             raise serializers.ValidationError("Parametro no existe")
 
     def buscar(self):
-        print("validated data:")
-        print(self.validated_data.get('genero'))
         libros = Libro.objects.filter(genero=self.validated_data.get('genero'))
-        print(libros)
-
         resp = LibroSerializer(libros, many=True).data
-        print(resp)
-
         return resp
